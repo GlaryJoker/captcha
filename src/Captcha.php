6c334ea538ff9343c6fed06592207a9db0757cfb
glaryjoker <glaryjoker@gmail.com>
2018-02-27 18:33:29 +0800

    初始化文件

---
+++ src/Captcha.php
@@ -0,0 +1,346 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/25
+ * Time: 16:10
+ */
+namespace glaryjoker\captcha;
+
+class Captcha{
+
+    protected $width = 240;
+
+    protected $height = 60;
+
+    protected $chineseString = '压押鸦鸭呀丫芽牙蚜崖衙涯雅哑亚讶焉咽阉烟淹盐严研蜒岩延言颜阎炎沿奄掩眼衍演艳堰燕厌砚雁唁彦焰宴谚验殃央鸯秧杨扬佯疡羊洋阳氧仰痒养样漾邀腰妖瑶摇尧遥窑谣姚咬舀药要耀椰噎耶爷野冶也页掖业叶曳腋夜液一壹医揖铱依伊衣颐夷遗移仪胰疑沂宜姨彝椅蚁倚已乙矣以艺抑易邑屹亿役臆逸肄疫亦裔意毅忆义益溢诣议谊译异翼翌绎茵荫因殷音阴姻吟银淫寅饮尹引隐印英樱婴鹰应缨莹萤营荧蝇迎赢盈影颖硬映哟拥佣臃痈庸雍踊蛹咏泳涌永恿勇用幽优悠忧尤由邮铀犹油游酉有友右佑釉诱又幼迂淤于盂榆虞愚舆余俞逾鱼愉渝渔隅予娱雨与屿禹宇语羽玉域芋郁吁遇喻峪御愈欲狱育誉浴寓裕预豫驭鸳渊冤元垣袁原援辕园员圆猿源缘远苑愿怨院曰约越跃钥岳粤月悦阅耘云郧匀陨允运蕴酝晕韵孕匝砸杂栽哉灾宰载再在咱攒暂赞赃脏葬遭糟凿藻枣早澡蚤躁噪造皂灶燥责择则泽贼怎增憎曾赠扎喳渣札轧铡闸眨栅榨咋乍炸诈摘斋宅窄债寨瞻毡詹粘沾盏斩辗崭展蘸栈占战站湛绽樟章彰漳张掌涨杖丈帐账仗胀瘴障招昭找沼赵照罩兆肇召遮折哲蛰辙者锗蔗这浙珍斟真甄砧臻贞针侦枕疹诊震振镇阵蒸挣睁征狰争怔整拯正政帧症郑证芝枝支吱蜘知肢脂汁之织职直植殖执值侄址指止趾只旨纸志挚掷至致置帜峙制智秩稚质炙痔滞治窒中盅忠钟衷终种肿重仲众舟周州洲诌粥轴肘帚咒皱宙昼骤珠株蛛朱猪诸诛逐竹烛煮拄瞩嘱主著柱助蛀贮铸筑住注祝驻抓爪拽专砖转撰赚篆桩庄装妆撞壮状椎锥追赘坠缀谆准捉拙卓桌琢茁酌啄着灼浊兹咨资姿滋淄孜紫仔籽滓子自渍字鬃棕踪宗综总纵邹走奏揍租足卒族祖诅阻组钻纂嘴醉最罪尊遵昨左佐柞做作坐座昔熙析西硒矽晰嘻吸锡牺稀息希悉膝夕惜熄烯溪汐犀檄袭席习媳喜铣洗系隙戏细瞎虾匣霞辖暇峡侠狭下厦夏吓掀锨先仙鲜纤咸贤衔舷闲涎弦嫌显险现献县腺馅羡宪陷限线相厢镶香箱襄湘乡翔祥详想响享项巷橡像向象萧硝霄削哮嚣销消宵淆晓小孝校肖啸笑效楔些歇蝎鞋协挟携邪斜胁谐写械卸蟹懈泄泻谢屑薪芯锌欣辛新忻心信衅星腥猩惺兴刑型形邢行醒幸杏性姓兄凶胸匈汹雄熊休修羞朽嗅锈秀袖绣墟戌需虚嘘须徐许蓄酗叙旭序畜恤絮婿绪续轩喧宣悬旋玄选癣眩绚靴薛学穴雪血勋熏循旬询寻驯巡殉汛训讯逊迅挖哇蛙洼娃瓦袜歪外豌弯湾玩顽丸烷完碗挽晚皖惋宛婉万腕汪王亡枉网往旺望忘妄威巍微危韦违桅围唯惟为潍维苇萎委伟伪尾纬未蔚味畏胃喂魏位渭谓尉慰卫瘟温蚊文闻纹吻稳紊问嗡翁瓮挝蜗涡窝我斡卧握沃巫呜钨乌污诬屋无芜梧吾吴毋武五捂午舞伍侮坞戊雾晤物勿务悟误塌他它她塔獭挞蹋踏胎苔抬台泰酞太态汰坍摊贪瘫滩坛檀痰潭谭谈坦毯袒碳探叹炭汤塘搪堂棠膛唐糖倘躺淌趟烫掏涛滔绦萄桃逃淘陶讨套特藤腾疼誊梯剔踢锑提题蹄啼体替嚏惕涕剃屉天添填田甜恬舔腆挑条迢眺跳贴铁帖厅听烃汀廷停亭庭挺艇通桐酮瞳同铜彤童桶捅筒统痛偷投头透凸秃突图徒途涂屠土吐兔湍团推颓腿蜕褪退吞屯臀拖托脱鸵陀驮驼椭妥拓唾撒洒萨腮鳃塞赛三叁伞散桑嗓丧搔骚扫嫂瑟色涩森僧莎砂杀刹沙纱傻啥煞筛晒珊苫杉山删煽衫闪陕擅赡膳善汕扇缮墒伤商赏晌上尚裳梢捎稍烧芍勺韶少哨邵绍奢赊蛇舌舍赦摄射慑涉社设砷申呻伸身深娠绅神沈审婶甚肾慎渗声生甥牲升绳省盛剩胜圣师失狮施湿诗尸虱十石拾时什食蚀实识史矢使屎驶始式示士世柿事拭誓逝势是嗜噬适仕侍释饰氏市恃室视试收手首守寿授售受瘦兽蔬枢梳殊抒输叔舒淑疏书赎孰熟薯暑曙署蜀黍鼠属术述树束戍竖墅庶数漱恕刷耍摔衰甩帅栓拴霜双爽谁水睡税吮瞬顺舜说硕朔烁斯撕嘶思私司丝死肆寺嗣四伺似饲巳松耸怂颂送宋讼诵搜艘擞嗽苏酥俗素速粟僳塑溯宿诉肃酸蒜算虽隋随绥髓碎岁穗遂隧祟孙损笋蓑梭唆缩琐索锁所然燃冉染瓤壤攘嚷让饶扰绕惹热壬仁人忍韧任认刃妊纫扔仍日戎茸蓉荣融熔溶容绒冗揉柔肉茹蠕儒孺如辱乳汝入褥软阮蕊瑞锐闰润若弱期欺栖戚妻七凄漆柒沏其棋奇歧畦崎脐齐旗祈祁骑起岂乞企启契砌器气迄弃汽泣讫掐洽牵扦钎铅千迁签仟谦乾黔钱钳前潜遣浅谴堑嵌欠歉枪呛腔羌墙蔷强抢橇锹敲悄桥瞧乔侨巧鞘撬翘峭俏窍切茄且怯窃钦侵亲秦琴勤芹擒禽寝沁青轻氢倾卿清擎晴氰情顷请庆琼穷秋丘邱球求囚酋泅趋区蛆曲躯屈驱渠取娶龋趣去圈颧权醛泉全痊拳犬券劝缺炔瘸却鹊榷确雀裙群啪趴爬帕怕琶拍排牌徘湃派攀潘盘磐盼畔判叛乓庞旁耪胖抛咆刨炮袍跑泡呸胚培裴赔陪配佩沛喷盆砰抨烹澎彭蓬棚硼篷膨朋鹏捧碰坯砒霹批披劈琵毗啤脾疲皮匹痞僻屁譬篇偏片骗飘漂瓢票撇瞥拼频贫品聘乒坪苹萍平凭瓶评屏坡泼颇婆破魄迫粕剖扑铺仆莆葡菩蒲埔朴圃普浦谱曝瀑妈麻玛码蚂马骂嘛吗埋买麦卖迈脉瞒馒蛮满蔓曼慢漫谩芒茫盲氓忙莽猫茅锚毛矛铆卯茂冒帽貌贸么玫枚梅酶霉煤没眉媒镁每美昧寐妹媚门闷们萌蒙檬盟锰猛梦孟眯醚靡糜迷谜弥米秘觅泌蜜密幂棉眠绵冕免勉娩缅面苗描瞄藐秒渺庙妙蔑灭民抿皿敏悯闽明螟鸣铭名命谬摸摹蘑模膜磨摩魔抹末莫墨默沫漠寞陌谋牟某拇牡亩姆母墓暮幕募慕木目睦牧垃拉喇蜡腊辣啦莱来赖蓝婪栏拦篮阑兰澜谰揽览懒缆烂滥琅榔狼廊郎朗浪捞劳牢老佬姥酪烙涝勒乐雷镭蕾磊累儡垒擂肋类泪棱楞冷厘梨犁黎篱狸离漓理李里鲤礼莉荔吏栗丽厉励砾历利僳例俐痢立粒沥隶力璃哩俩联莲连镰廉怜涟帘敛脸链恋炼练粮凉梁粱良两辆量晾亮谅撩聊僚疗燎寥辽潦了撂镣廖料列裂烈劣猎琳林磷霖临邻鳞淋凛赁吝拎玲菱零龄铃伶羚凌灵陵岭领另令溜琉榴硫馏留刘瘤流柳六龙聋咙笼窿隆垄拢陇楼娄搂篓漏陋芦卢颅庐炉掳卤虏鲁麓碌露路赂鹿潞禄录陆戮驴吕铝侣旅履屡缕虑氯律率滤绿峦挛孪滦卵乱掠略抡轮伦仑沦纶论萝螺罗逻锣箩骡裸落洛骆络喀咖卡咯开揩楷凯慨刊堪勘坎砍看康慷糠扛抗亢炕考拷烤靠坷苛柯棵磕颗科壳咳可渴克刻客课肯啃垦恳坑吭空恐孔控抠口扣寇枯哭窟苦酷库裤夸垮挎跨胯块筷侩快宽款匡筐狂框矿眶旷况亏盔岿窥葵奎魁傀馈愧溃坤昆捆困括扩廓阔击圾基机畸稽积箕肌饥迹激讥鸡姬绩缉吉极棘辑籍集及急疾汲即嫉级挤几脊己蓟技冀季伎祭剂悸济寄寂计记既忌际妓继纪嘉枷夹佳家加荚颊贾甲钾假稼价架驾嫁歼监坚尖笺间煎兼肩艰奸缄茧检柬碱碱拣捡简俭剪减荐槛鉴践贱见键箭件健舰剑饯渐溅涧建僵姜将浆江疆蒋桨奖讲匠酱降蕉椒礁焦胶交郊浇骄娇嚼搅铰矫侥脚狡角饺缴绞剿教酵轿较叫窖揭接皆秸街阶截劫节桔杰捷睫竭洁结解姐戒藉芥界借介疥诫届巾筋斤金今津襟紧锦仅谨进靳晋禁近烬浸尽劲荆兢茎睛晶鲸京惊精粳经井警景颈静境敬镜径痉靖竟竞净炯窘揪究纠玖韭久灸九酒厩救旧臼舅咎就疚鞠拘狙疽居驹菊局咀矩举沮聚拒据巨具距踞锯俱句惧炬剧捐鹃娟倦眷卷绢撅攫抉掘倔爵觉决诀绝均菌钧军君峻俊竣浚郡骏哈骸孩海氦亥害骇酣憨邯韩含涵寒函喊罕翰撼捍旱憾悍焊汗汉夯杭航壕嚎豪毫郝好耗号浩呵喝荷菏核禾和何合盒貉阂河涸赫褐鹤贺嘿黑痕很狠恨哼亨横衡恒轰哄烘虹鸿洪宏弘红喉侯猴吼厚候后呼乎忽瑚壶葫胡蝴狐糊湖弧虎唬护互沪户花哗华猾滑画划化话槐徊怀淮坏欢环桓还缓换患唤痪豢焕涣宦幻荒慌黄磺蝗簧皇凰惶煌晃幌恍谎灰挥辉徽恢蛔回毁悔慧卉惠晦贿秽会烩汇讳诲绘荤昏婚魂浑混豁活伙火获或惑霍货祸噶嘎该改概钙盖溉干甘杆柑竿肝赶感秆敢赣冈刚钢缸肛纲岗港杠篙皋高膏羔糕搞镐稿告哥歌搁戈鸽胳疙割革葛格蛤阁隔铬个各给根跟耕更庚羹埂耿梗工攻功恭龚供躬公宫弓巩汞拱贡共钩勾沟苟狗垢构购够辜菇咕箍估沽孤姑鼓古蛊骨谷股故顾固雇刮瓜剐寡挂褂乖拐怪棺关官冠观管馆罐惯灌贯光广逛瑰规圭硅归龟闺轨鬼诡癸桂柜跪贵刽辊滚棍锅郭国果裹过发罚筏伐乏阀法珐藩帆番翻樊矾钒繁凡烦反返范贩犯饭泛坊芳方肪房防妨仿访纺放菲非啡飞肥匪诽吠肺废沸费芬酚吩氛分纷坟焚汾粉奋份忿愤粪丰封枫蜂峰锋风疯烽逢冯缝讽奉凤佛否夫敷肤孵扶拂辐幅氟符伏俘服浮涪福袱弗甫抚辅俯釜斧脯腑府腐赴副覆赋复傅付阜父腹负富讣附妇缚咐搭达答瘩打大呆歹傣戴带殆代贷袋待逮怠耽担丹单郸掸胆旦氮但惮淡诞弹蛋当挡党荡档刀捣蹈倒岛祷导到稻悼道盗德得的蹬灯登等瞪凳邓堤低滴迪敌笛狄涤翟嫡抵底地蒂第帝弟递缔颠掂滇碘点典靛垫电佃甸店惦奠淀殿碉叼雕凋刁掉吊钓调跌爹碟蝶迭谍叠丁盯叮钉顶鼎锭定订丢东冬董懂动栋侗恫冻洞兜抖斗陡豆逗痘都督毒犊独读堵睹赌杜镀肚度渡妒端短锻段断缎堆兑队对墩吨蹲敦顿囤钝盾遁掇哆多夺垛躲朵跺舵剁惰堕擦猜裁材才财睬踩采彩菜蔡餐参蚕残惭惨灿苍舱仓沧藏操糙槽曹草厕策侧册测层蹭插叉茬茶查碴搽察岔差诧拆柴豺搀掺蝉馋谗缠铲产阐颤昌猖场尝常长偿肠厂敞畅唱倡超抄钞朝嘲潮巢吵炒车扯撤掣彻澈郴臣辰尘晨忱沉陈趁衬撑称城橙成呈乘程惩澄诚承逞骋秤吃痴持匙池迟弛驰耻齿侈尺赤翅斥炽充冲虫崇宠抽酬畴踌稠愁筹仇绸瞅丑臭初出橱厨躇锄雏滁除楚础储矗搐触处揣川穿椽传船喘串疮窗幢床闯创吹炊捶锤垂春椿醇唇淳纯蠢戳绰疵茨磁雌辞慈瓷词此刺赐次聪葱囱匆从丛凑粗醋簇促蹿篡窜摧崔催脆瘁粹淬翠村存寸磋撮搓措挫错芭捌扒叭吧笆八疤巴拔跋靶把耙坝霸罢爸白柏百摆佰败拜稗斑班搬扳般颁板版扮拌伴瓣半办绊邦帮梆榜膀绑棒磅蚌镑傍谤苞胞包褒剥薄雹保堡饱宝抱报暴豹鲍爆杯碑悲卑北辈背贝钡倍狈备惫焙被奔苯本笨崩绷甭泵蹦迸逼鼻比鄙笔彼碧蓖蔽毕毙毖币庇痹闭敝弊必辟壁臂避陛鞭边编贬扁便变卞辨辩辫遍标彪膘表鳖憋别瘪彬斌濒滨宾摈兵冰柄丙秉饼炳病并玻菠播拨钵波博勃搏铂箔伯帛舶脖膊渤泊驳捕卜哺补埠不布步簿部怖啊阿埃挨哎唉哀皑癌蔼矮艾碍爱隘鞍氨安俺按暗岸胺案肮昂盎凹敖熬翱袄傲奥懊澳';
+
+    protected $numberWithLetter = '123456789abcdefghijklmnopkrstuvwxyzABCDEFGHIJKLMNOPKRSTUVWXYZ';
+
+    //长度
+    protected $length;
+
+    //画布
+    protected $canvas;
+
+    //验证码名称
+    protected $name;
+
+    //是否加密
+    protected $crypt = true;
+
+    //线的数量
+    protected $lineNumber = 5;
+
+    //点的数量
+    protected $dotNumber = 20;
+
+    protected $fontSize =18;
+
+    protected $specialStringNumber = 0;
+
+    protected $language;
+
+    //验证方式
+    /**
+     * @var int 0 或者 1  0是输入所有的字符 1 是输入非倒立的文字 2输入倒立的文字 只有 中文的时候才 倒立文字才有效
+     */
+    protected $way;
+
+    protected $tips = [
+        "请按从左到右的顺序输入图中文字",
+        "请按从左到右的顺序输入非倒立文字",
+        "请按从左到右的顺序点击倒立文字",
+    ];
+
+
+    public function __construct(array $args = [])
+    {
+
+        $default = [
+            'name' => '_captcha',
+            'length' => 8,
+            'specialStringNumber' => 2,
+            'hasSpecialString' => false,
+            'language' => 'chinese',
+            'way' => 0
+        ];
+
+        $config = array_merge($default,$args);
+
+        $this->name = $config['name'];
+
+        $this->length = $config['length'];
+
+        $this->language = in_array($config['language'],['chinese','english'])?$config['language']:'chinese';
+
+        $this->specialStringNumber = is_int($config['specialStringNumber']) && $config['specialStringNumber'] > 0 && $config['specialStringNumber']<$this->length ? $config['specialStringNumber'] : 0;
+        $this->way = $config['way']<count($this->tips)?$config['way']:0;
+
+        if($this->language === 'english'){
+            $this->way = 0;
+        }
+
+
+    }
+
+
+    protected function dot(){
+
+        for($i=0;$i<$this->dotNumber;$i++){
+            $color = imagecolorallocate($this->canvas,rand(100,200),rand(100,200),rand(100,200));
+            $x = $this->width-1;
+            $y = $this->height-1;
+            imagesetpixel($this->canvas,rand(1,$x),rand(1,$y),$color);
+        }
+
+    }
+
+    protected function line(){
+        if($this->lineNumber > 0){
+            for($i=0;$i<$this->lineNumber;$i++){
+                $color = imagecolorallocate($this->canvas,rand(100,200),rand(100,200),rand(100,200));
+                $x = $this->width-1;
+                $y = $this->height-1;
+                imageline($this->canvas,rand(1,$x),rand(1,$y),rand(1,$x),rand(1,$y),$color);
+            }
+        }
+    }
+
+    //创建画布
+    protected function createCanvas(){
+        $this->canvas = @imagecreatetruecolor($this->width, $this->height)
+        or die('Cannot Initialize new GD image stream');
+        $white = imagecolorallocate($this->canvas, 255,255,255);
+        $blue = imagecolorallocate($this->canvas, mt_rand(100,255), mt_rand(100,255), mt_rand(100,255));
+        $pink = imagecolorallocate($this->canvas, mt_rand(100,255), mt_rand(100,255), mt_rand(100,255));
+        $bg = imagecolorallocate($this->canvas, mt_rand(100,255), mt_rand(100,255), mt_rand(100,255));
+        imagefill($this->canvas,0,0,$white);
+    }
+
+    //返回倒立字体的索引
+    protected function specialStringIndex(){
+
+        $specialString = [];
+        for($j=0;$j<$this->specialStringNumber;$j++){
+            $specialString[] = mt_rand(0,$this->length-1);
+        }
+
+
+        //如果出现的随机数重复 则数组长度为1 再次随机
+        $count = count(array_count_values($specialString));
+
+        if($count <$this->specialStringNumber){
+            return $this->specialStringIndex();
+        }else{
+            return $specialString;
+        }
+    }
+
+    //字符串
+    protected function getString(){
+        $string = '';
+        if($this->language === 'chinese'){
+            $sourceString = $this->chineseString;
+        }else{
+            $sourceString = $this->numberWithLetter;
+        }
+
+
+        for($i=0;$i<$this->length;$i++){
+            $string .= mb_substr($sourceString,mt_rand(0,mb_strlen($sourceString)-1),1);
+        }
+        return $string;
+    }
+
+    //渲染字符串
+    protected function createString(){
+        $specialStringArr = $this->specialStringIndex();
+
+        $finalString = $this->getString();
+        $stringLength = mb_strlen($this->getString());
+
+        $sessionString = '';
+        $finalSessionString = '';
+
+        //输入非倒立的文字 把非倒立的文字存入session
+        if($this->way === 1){
+            $finalSessionString = $finalString;
+
+            for($i=0;$i<$stringLength;$i++){
+
+                foreach ($specialStringArr as $index){
+                    if(mb_substr($finalString,$i,1) === mb_substr($finalString,$index,1)){
+                        $sessionString .= mb_substr($finalString,$i,1);
+                    }
+                }
+            }
+
+            for($j=0;$j<$this->specialStringNumber;$j++){
+                //去除倒立的文字 剩余的非倒立的文字
+                $finalSessionString = str_replace(mb_substr($sessionString,$j,1),'',$finalSessionString);
+            }
+        }
+
+        if($this->way === 2){
+            $finalSessionString = [];
+        }
+
+        $font = './font/c.ttf';
+
+
+        for($i=0;$i<$this->length;$i++){
+            $color = imagecolorallocate($this->canvas,rand(0,150),rand(0,150),rand(0,150));
+            $angle = 0;
+            $x = ($this->width/$this->length)*$i;
+            $y = 25+20;
+            if($this->language === 'chinese'){
+                if($this->specialStringNumber > 0){
+                    //有倒立文字
+                    if(in_array($i,$specialStringArr)){
+                        $angle = 180;
+
+                        $x = ($this->width/$this->length)*($i+1);
+
+                        $y = $this->height-$this->fontSize-13;
+
+                        if($this->way === 2){
+                            //存入倒立文字的x和y轴坐标
+                            $fontSizeHalfX = $this->fontSize/2;
+                            $fontSizeHalfY = $this->fontSize/2+10;
+                            //x轴的最小值
+                            $finalXMin = $x-$fontSizeHalfX-$this->fontSize;
+                            //x轴最大值
+                            $finalXMax = $x+$fontSizeHalfX;
+
+                            $finalYMin = $y-$fontSizeHalfY;
+                            $finalYMax = $y+$fontSizeHalfY;
+
+                            $finalSessionString[] = [
+                                'x' => [
+                                    'min' => $finalXMin,
+                                    'max' => $finalXMax
+                                ],
+                                'y' =>[
+                                    'min' => $finalYMin,
+                                    'max' => $finalYMax
+                                ]
+                            ];
+                        }
+                    }
+                }
+            }
+
+
+
+            $w = mb_substr($finalString,$i,1);
+
+            imagettftext($this->canvas, $this->fontSize, $angle, $x, $y, $color, $font, mb_convert_encoding($w,  'html-entities', 'UTF-8'));
+
+        }
+
+        $color = imagecolorallocate($this->canvas,0,0,0);
+
+
+        imagettftext($this->canvas, 12, 0, 0, 15, $color, $font, mb_convert_encoding($this->tips[$this->way],  'html-entities', 'UTF-8'));
+
+
+        $this->saveSession($finalSessionString);
+    }
+
+    //存取session
+    protected function saveSession($sessionData){
+
+        $_SESSION[$this->name] = $sessionData;
+
+    }
+
+    public function getSession($name = ""){
+        if($name === ""){
+            $name = $this->name;
+        }
+        return isset($_SESSION[$name])?$_SESSION[$name]:false;
+    }
+
+    /**
+     * @param $captcha 1 传过来的验证码数据
+     * @param $name 2 session名称
+     * @param $way 3 验证方式
+     * @return bool
+     */
+     public function checkSession($captcha,$name){
+
+         if($captcha === '' || $captcha === NULL){
+             return false;
+         }
+         $theSession = $this->getSession($name);
+
+         if(!$theSession){
+             return false;
+         }
+         //如果session是数组  则进行 坐标验证 否则 进行
+         if(is_array($theSession)){
+             /**
+              * 点击验证 数据格式 x1-y1,x2-y2,x3-y3
+              */
+
+             $result = 0;
+             $captcha = explode(',',$captcha);
+
+             if(count($captcha) > count($theSession)){
+                 return false;
+             }
+
+             foreach($theSession as $key => $value){
+                 $theData = explode('-',$captcha[$key]);
+                 if($theData[0] > $theSession[$key]['x']['min'] && $theData[0] < $theSession[$key]['x']['max']){
+                     if($theData[1] > $theSession[$key]['y']['min'] && $theData[1] < $theSession[$key]['y']['max']){
+                         $result++;
+                     }
+                 }
+             }
+
+             if($result === count($theSession)){
+
+                 $bool = true;
+             }else{
+                 $bool = false;
+             }
+         }else{
+             if(strtolower($theSession) === strtolower($captcha)){
+                 $bool = true;
+             }else{
+                 $bool = false;
+             }
+         }
+         if($bool){
+             $this->clearSession($name);
+         }
+
+         return $bool;
+    }
+
+    protected function clearSession($name){
+
+        unset($_SESSION[$name]);
+    }
+
+    public function output(){
+        //创建画布
+        $this->createCanvas();
+
+        //创建点
+        $this->dot();
+
+        //创建线
+        $this->line();
+
+        //文字
+        $this->createString();
+
+
+        header("Content-Type:image/png");
+        imagepng($this->canvas);
+    }
+
+}
